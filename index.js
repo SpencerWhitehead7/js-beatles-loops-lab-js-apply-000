fc90421fb4a555c669057c5376d44efa27e764c0
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-02-18 23:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(beatles, instruments){
   var out = []
-  for(i=0; i<beatles.length; i++){
+  for(var i=0; i<beatles.length; i++){
     out.push(`beatles[i] plays instruments[i]`)
   }
   return out
